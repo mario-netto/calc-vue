ce4bae39b0ca0261a443e9cbea0f3804f699499d
Mario Netto <marionettojr@gmail.com>
2024-11-21 13:31:59 -0300

    logica e funcao calculo ok

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,141 @@
 <script setup>
+    import { reactive } from 'vue';
+
+    const estado = reactive({
+        calcSelect: 'soma',
+        campo1: 0,
+        campo2: 0,
+        resultado: 0,
+    });
+
+    function calc() {
+        switch (estado.calcSelect) {
+            case 'soma':
+                return estado.campo1 + estado.campo2;
+            case 'sub':
+                return estado.campo1 - estado.campo2;
+            case 'mult':
+                return estado.campo1 * estado.campo2;
+            case 'div': 
+                return estado.campo1 / estado.campo2;
+        }
+    }
 </script>
 
 <template>
-  <!-- header  -->
-  <header>
-    <div class="container">
-      <img class="img-calcae" src="./assets/calcae.png" alt="">
-      <img class="img-calc" src="./assets/calc.png" alt="">
-    </div>
-  </header>
-  
-  <!-- área de input e calc -->
-  <section>
-    <div class="custom-shape">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" class="shape-fill"></path>
-      </svg>
-  </div>
-    <div class="section">
-      
-    </div>
-  </section>
+    <body>
+        <!-- header  -->
+        <header>
+            <div class="container">
+                <img class="img-calcae" src="./assets/calcae.png" alt="">
+                <img class="img-calc" src="./assets/calc.png" alt="">
+            </div>
+        </header>
+        <!-- área de input e calc -->
+        <section>
+            <div class="custom-shape">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                    preserveAspectRatio="none">
+                    <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" class="shape-fill"></path>
+                </svg>
+            </div>
+            <div class="section container">
+                <div class="form-content">
+                    <form>
+                        <input @keyup="evento => estado.campo1 = parseFloat(evento.target.value)" type="number" placeholder="0" value="0">
+                        <select @change="evento => estado.calcSelect = evento.target.value" >
+                            <option value="soma">+</option>
+                            <option value="sub">-</option>
+                            <option value="mult">*</option>
+                            <option value="div">÷</option>
+                        </select>
+                        <input @keyup="evento => estado.campo2 = parseFloat(evento.target.value)" type="number" placeholder="0" value="0">
+                    </form>
+                    <span class="resultado"> {{ calc() }} </span>
+                </div>
+            </div>
+        </section>
+    </body>
 </template>
 
 <style scoped>
-  * {
+* {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-  }
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
 
-  .container {
+body {
+    background-color: #F5CB47;
+    height: 100vh;
+}
+
+.container {
     max-width: 1024px;
     margin: 0 auto;
-  }
+}
 
-  header {
+header {
     display: flex;
     justify-content: center;
     background-color: #F196E5;
     padding: 16px 0;
-  }
+}
 
-  .img-calcae {
+.img-calcae {
     max-width: 500px;
-  }
+}
 
-  .img-calc {
+.img-calc {
     max-width: 300px;
     margin-right: 26px;
     margin-top: 16px;
-  }
+}
 
-  section {
+section {
     position: relative;
     background-color: #F5CB47;
-    height: 100vh;
-  }
+    /* height: 59vh; */
+}
 
-  .custom-shape{
+.section {
+    padding-top: 64px;
+}
+
+.form-content {
+    text-align: center;
+}
+
+.resultado {
+    display: block;
+    font-size: 3em;
+    font-weight: bold;
+    margin-top: 24px;
+    padding-bottom: 24px;
+}
+
+.text-content {
+    margin-top: 60px;
+}
+
+.text-content h1 {
+    margin-bottom: 8px;
+}
+
+input,
+select {
+    width: 64px;
+    height: 40px;
+    border-radius: 8px;
+    border: none;
+}
+
+select {
+    margin: 0 32px;
+}
+
+.custom-shape {
     position: absolute;
     top: 0;
     left: 0;
@@ -72,8 +149,7 @@
     position: relative;
     display: block;
     width: calc(100% + 1.3px);
-    height: 127px;
-    transform: rotateY(180deg);
+    height: 32px;
 }
 
 .custom-shape .shape-fill {
